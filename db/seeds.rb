c75fcb479d72a59f74bbb5e2c93852d1fccf2367
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-01-18 11:34:53 -0500

    commit tree 1bee8e52e781888d9be336bc58382a1df99046d5

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artists = Artist.create([
+  {name: "Joni Mitchell", bio: "Joni's bio"},
+  {name: "Neil Young", bio: "Neil's bio"},
+  {name: "Elvis Costello", bio: "Elvis's bio"},
+  {name: "Madonna", bio: "Madonna's bio"},
+  {name: "MC Lyte", bio: "MC Lyte's bio"},
+
+])
+
+genres = Genre.create([
+  {name: "Folk"},
+  {name: "Rock"},
+  {name: "Pop"},
+  {name: "Rap"},
+  {name: "Alternative"},
+])
+
+songs = Song.create([
+  {name: "Vogue", artist_id: 4, genre_id: 3},
+  {name: "Material Girl", artist_id: 4, genre_id: 3},
+  {name: "Keep on Rockin in the Free World", artist_id: 2, genre_id: 2},
+  {name: "Allison", artist_id: 3, genre_id: 2},
+  {name: "Rock the Party", artist_id: 5, genre_id: 4},
+  {name: "Big Yellow Taxi", artist_id: 1, genre_id: 1},
+  {name: "California", artist_id: 1, genre_id: 1}
+])
